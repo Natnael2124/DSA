c11b4b9f29d96b8b0bf8f6499cebb20a341fa37f
Natnael Gemechu <natnaelbajo24@gmail.com>
2025-11-18 22:24:07 +0300

    Time: 15 ms (57.04%) | Memory: 17.9 MB (63.54%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -11,8 +11,5 @@ class Solution:
                 x+=1
             else:
                 x-=1
-        if x==0 and y==0:
-            return True
-        else:
-            return False
+        return x==0 and y==0
         
