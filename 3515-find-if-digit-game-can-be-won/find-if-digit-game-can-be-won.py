3a96d205ceda5584e67bac30e790aa55df0342ed
Natnael Gemechu <natnaelbajo24@gmail.com>
2025-11-18 22:37:37 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.46%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.py
@@ -1,7 +1,6 @@
 class Solution:
     def canAliceWin(self, nums: List[int]) -> bool:
-        two = 0 
-        one = 0
+        two = one =0 
         for i in nums:
             if i < 10:
                 one += i
