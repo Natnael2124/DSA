3445aed3a12aae8fe23b89c9a29b87e1e483e0b1
Natnael Gemechu <natnaelbajo24@gmail.com>
2025-11-21 22:15:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (87.00%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,16 +1,15 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = [[1]]  # first row
+        res = [[1]]  
 
         for i in range(1, numRows):
             prev = res[-1]
-            row = [1]  # first element
+            row = [1]  
             
-            # middle elements
             for j in range(1, i):
                 row.append(prev[j-1] + prev[j])
             
-            row.append(1)  # last element
+            row.append(1)  
             res.append(row)
         
         return res
