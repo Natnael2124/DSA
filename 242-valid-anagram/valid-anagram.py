c721d160d57c0df695f1c9255e3e202a3546a869
Natnael Gemechu <natnaelbajo24@gmail.com>
2026-02-03 15:03:09 +0300

    Time: 9 ms (79.20%) | Memory: 19.6 MB (22.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,16 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        from collections import Counter
+        count1=Counter(s)
+        count2=Counter(t)
+        if len(s)!= len(t):
             return False
-
-        count_s = {}
-        count_t = {}
-
-        for ch in s:
-            count_s[ch] = count_s.get(ch, 0) + 1
-
-        for ch in t:
-            count_t[ch] = count_t.get(ch, 0) + 1
-
-        return count_s == count_t
-        
+        for i in range(len(s)):
+            if count1[s[i]]==count2[s[i]] and count1[t[i]]==count2[t[i]]  :
+                continue
+            else:
+                return False
+        return True
