465032890fdef4d1a29b99161ba45295a1100bbb
Natnael Gemechu <natnaelbajo24@gmail.com>
2026-04-01 17:25:41 +0300

    Time: 15 ms (5.41%) | Memory: 19.6 MB (15.37%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def sub(i,num):
+            if num not in res:
+                sore = sorted(num)
+                res.append(sore)
+
+            if i == len(nums) :
+                return
+            
+            for j in range(i,len(nums)):
+                num.append(nums[j])
+                i += 1
+                sub(i,num)
+                num.pop()
+                
+
+
+
+        sub(0,[])
+        res.sort()
+        p = []
+        for i in res:
+            if i not in p:
+                p.append(i)
+        return p
