3d46e7a7b705381df27f8d0d9f476b09d5184596
Natnael Gemechu <natnaelbajo24@gmail.com>
2026-04-21 16:50:51 +0300

    Time: 126 ms (66.28%) | Memory: 20.6 MB (94.50%) - LeetSync

--- 690-employee-importance/employee-importance.py
+++ 690-employee-importance/employee-importance.py
@@ -1,15 +1,19 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        
-        emp_map = {emp.id: emp for emp in employees}
-        
-        def dfs(emp_id):
-            emp = emp_map[emp_id]
-            total = emp.importance
-            
-            for sub_id in emp.subordinates:
-                total += dfs(sub_id)
-            
-            return total
-        
-        return dfs(id)
+        emp_map = {emp.id : emp for emp in employees}
+        total = 0
+        stack = [id]
+        while stack:
+            em = emp_map[stack.pop()]
+            total += em.importance
+            stack += em.subordinates
+        return total
