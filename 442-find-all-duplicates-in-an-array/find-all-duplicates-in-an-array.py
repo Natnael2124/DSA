e8d2ab91e73797901df9d14567bb136c6b82ffb6
Natnael Gemechu <natnaelbajo24@gmail.com>
2026-04-23 13:57:45 +0300

    Time: 31 ms (62.48%) | Memory: 29.6 MB (83.74%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,10 +1,13 @@
 class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        from collections import Counter
-        count=Counter(nums)
-        output=[]
-        for i in count:
-            if count[i]>1:
-                output.append(i)
-        return output
-        
+    def findDuplicates(self, nums: list[int]) -> list[int]:
+        res = []
+        for x in nums:
+            # Use the value as an index (mapped to 0-based)
+            idx = abs(x) - 1
+            if nums[idx] < 0:
+                # If already negative, we've seen this value before
+                res.append(abs(x))
+            else:
+                # Mark as seen by negating the value at that index
+                nums[idx] *= -1
+        return res
