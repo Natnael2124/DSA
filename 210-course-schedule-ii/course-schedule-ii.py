32bd7c6ffb31694b7526cefd21a09675fec514ae
Natnael Gemechu <natnaelbajo24@gmail.com>
2026-04-29 18:59:15 +0300

    Time: 0 ms (100.00%) | Memory: 20.2 MB (90.22%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,36 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
+        indegree = [0]*numCourses
 
-        for course, preq in prerequisites:
-            graph[preq].append(course)
+        for a,b in prerequisites:
+            graph[b].append(a)
+            indegree[a] +=1
+        
+        # start processing nodes that have 0 degree
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        visited = 0
+        order = []
+        while queue:
+            node = queue.popleft()
+            visited += 1
+            order.append(node)
 
-        color = [0] * numCourses
-        res = []
-
-        def dfs(node):
-            if color[node] == 1:
-                return False
-            
-            if color[node] == 2:
-                return True
-            
-            color[node] = 1
-            
-            for i in graph[node]:
-                if not dfs(i):
-                    return False
-
-            color[node] = 2
-            res.append(node)
-            return True
-
-        for j in range(numCourses):
-            if color[j] == 0:
-                if not dfs(j):
-                    return []
-
-        return res[::-1]
-
+            for nei in graph[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        
+        if visited == numCourses:
+            return order
+        return []
             
 
