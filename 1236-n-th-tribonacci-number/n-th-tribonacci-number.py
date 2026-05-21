758e392ae99c5f980e660990ffc5aeb234eeb81f
Natnael Gemechu Bajo <natnaelbajo24@gmail.com>
2026-05-21 16:31:18 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.96%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -5,13 +5,10 @@ class Solution:
         if n == 1 or n == 2:
             return 1
         
-        # Initialize the first three terms
         t0, t1, t2 = 0, 1, 1
         
-        # Iteratively calculate up to n
         for _ in range(3, n + 1):
             next_t = t0 + t1 + t2
-            # Shift our window forward
             t0, t1, t2 = t1, t2, next_t
             
         return t2
